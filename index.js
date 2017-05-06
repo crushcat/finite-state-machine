76d65ab6eb9cc205542ffdf37b8db3bde958e745
crushcat <crushcat@gmail.com>
2017-05-06 12:22:20 +0300

    task implementation

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 const FSM = require('./src/fsm.js');
-
-window.FSM = FSM;
+const config = {
+    initial: 'normal',
+    states: {
+        normal: {
+            transitions: {
+                study: 'busy',
+            }
+        },
+        busy: {
+            transitions: {
+                get_tired: 'sleeping',
+                get_hungry: 'hungry',
+            }
+        },
+        hungry: {
+            transitions: {
+                eat: 'normal'
+            },
+        },
+        sleeping: {
+            transitions: {
+                get_hungry: 'hungry',
+                get_up: 'normal',
+            },
+        },
+    }
+};
+window.FSM = new FSM(conf);
--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,91 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+      if(!config){throw new Error("gimme config!");}
+      //DBG field
+      this._config = config;
+      //----
+
+      this._initialState = config.initial;
+      this._currentState = this._initialState;
+      this._prevState = null;
+      this._canUndo = false;
+      this._canRedo = false;
+      this.uFlg = false;
+      this.rFlg = false;
+
+    }
+
+    _allStates() {
+      let arr = [];
+      for (var key in this._config.states){
+        arr.push(key)
+      }
+      return arr;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      return this._currentState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+      if(this._config.states.hasOwnProperty(state)){
+        this._prevState = this._currentState;
+        //call from undo
+        if(this.uFlg){
+            this._canUndo = false;
+            this._canRedo = true;
+        }
+        //call from redo
+        else if(this.rFlg){
+          this._canUndo = true;
+          this._canRedo = false;
+        }
+        else{
+          this._canUndo = true;
+          this._canRedo = false;
+        }
+
+        this._currentState = state;
+        this.uFlg = false;
+        this.rFlg = false;
+      }
+      else {throw new Error("no such state");}
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      if(this._config.states[this._currentState].transitions.hasOwnProperty(event)){
+        this.changeState(this._config.states[this._currentState].transitions[event])
+      }else{
+          throw new Error("no such event");
+      }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+      this.changeState(this._initialState);
+      this._prevState = null;
+      this._canUndo = false;
+      this._canRedo = false;
+      this.uFlg = false;
+      this.rFlg = false;
+
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +95,69 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+      if(event /*TODO check event validity*/){
+        let eStates = [];
+        for (let state in this._config.states){
+          if(this._config.states[state].transitions.hasOwnProperty(event)){
+            eStates.push(state);
+          }
+        }
+        return eStates;
+      }else {
+        return this._allStates();
+      }
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+      if(this._canUndo){
+        this._canUndo = false;
+        this._canRedo = true;
+        this.uFlg = true;
+        this.changeState(this._prevState);
+        return true;
+      }else {
+        return false;
+      }
+
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+      if(this._canRedo){
+        // if(this._currentState == this._prevState){
+        //   this._prevState = null;
+        // }
+        this._canUndo = true;
+        this._canRedo = false;
+        this.rFlg = true;
+        this.changeState(this._prevState);
+
+        return true;
+      }else {
+        return false;
+      }
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+      this._prevState = null;
+      this._canUndo = false;
+      this._canRedo = false;
+      this.uFlg = false;
+      this.rFlg = false;
+    }
 }
 
 module.exports = FSM;
